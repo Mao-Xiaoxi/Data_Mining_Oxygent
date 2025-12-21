0f4a45ade8b340084b69f96edb97b288ff56cfa6
MaoXiaoxi <maoxiaoxi137@gmail.com>
2025-12-21 12:26:07 +0800

    添加了MCP调用示例：（注意oxy_space中修改可访问文件夹路径）。 再次进行了问题测试：可回答问题7/100，其中网访问功能存在问题，对于大量问题无法正确运行，目前猜测并行问题

--- demo.py
+++ demo.py
@@ -39,18 +39,45 @@ oxy_space = [
    #    desc="A tool that can operate the file system", #可以设置prompt
    #    tools=["file_tools"],
    # ),
-   preset_tools.math_tools,
-   oxy.ReActAgent(
-      name="math_agent",
-      desc="A tool that can perform mathematical calculations.",
-      tools=["math_tools"],
+
+   # 使用mcp需要删除代码中重名function call（包括master中的subagent）
+   # preset_tools.math_tools,
+   # oxy.ReActAgent(
+   #    name="math_agent",
+   #    desc="A tool that can perform mathematical calculations.",
+   #    tools=["math_tools"],
+   # ),
+
+   # 调用MCP工具接口
+   oxy.StdioMCPClient(
+      name="math_tools",
+      params={
+         "command": "uv",
+         "args": ["--directory", "./mcp_servers", "run", "math_tools.py"],
+      },
    ),
-   tools.file_tools,
-   oxy.ReActAgent(
-      name="my_file_agent",
-      tools=["file_tools"],
-      llm_model="default_llm",
+   oxy.StdioMCPClient(
+      name="fetch_tools",
+      params={
+         "command": "npx",
+         "args": ["-y", "@guhcostan/web-search-mcp@latest"],
+      }
    ),
+   oxy.StdioMCPClient(
+      name="file_tools",
+      params={
+         "command": "npx",
+         "args": ["-y", "@modelcontextprotocol/server-filesystem", "d:\Python\Data_Mining_Oxygent"],
+         "disabled": "false",
+         "autoApprove": []
+      }
+   ),
+   # tools.file_tools,
+   # oxy.ReActAgent(
+   #    name="my_file_agent",
+   #    tools=["file_tools"],
+   #    llm_model="default_llm",
+   # ),
    my_first_tools.mysterious_tools,
    oxy.ReActAgent( # our own agent
       name="mysterious_agent",
@@ -60,14 +87,17 @@ oxy_space = [
    oxy.ReActAgent(
       is_master=True,
       name="master_agent",
-      sub_agents=["time_agent", "math_agent","my_file_agent","mysterious_agent"],
+      tools=["math_tools","fetch_tools","file_tools"],
+      llm_model="default_llm",
+      sub_agents=["time_agent","mysterious_agent"], # ,"my_file_agent"
    )
 ]
 
 async def main():
    async with MAS(oxy_space=oxy_space) as mas:
+      #await mas.start_web_service(
       await mas.start_cli_mode(
-         first_query="please,print the mysterious information."
+         first_query="please,print the mysterious information"
       )
 
 if __name__ == "__main__":
---
+++ mcp_servers/fetch_tools.py
@@ -0,0 +1,6 @@
+from decimal import Decimal, getcontext
+
+from mcp.server.fastmcp import FastMCP
+from pandas import DataFrame
+
+mcp=FastMCP()
